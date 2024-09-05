538002431e8591475ccbaade61c8959c4e5302ff
kdjack <501703793@qq.com>
2024-09-05 16:03:10 +0800

    修改label

--- lib/components/el-plus-form/mixins/index.ts
+++ lib/components/el-plus-form/mixins/index.ts
@@ -46,7 +46,7 @@ export const getAttrs = async (props: IMixinsProps, customAttrs?: { [key: string
     }
   }
 
-  if (['input', 'switch', 'number', 'nbinput', 'checkbox', 'date', 'daterange'].indexOf(props.desc.type) >= 0) {
+  if (['area', 'input', 'switch', 'number', 'nbinput', 'checkbox', 'date', 'daterange', 'datetime', 'percentinput'].indexOf(props.desc.type) >= 0) {
     delete attrs.label
   }
   return attrs

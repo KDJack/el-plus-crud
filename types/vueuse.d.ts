46dfa0e9316af1d71d0a6d2d301013af3e5aea44
kdjack <501703793@qq.com>
2026-06-11 09:06:01 +0800

    修改打包配置

--- types/vueuse.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@vueuse/core' {
-  import { Ref, WritableComputedRef } from 'vue'
-  export function useVModel<T>(props: any, key: string, emit: any): Ref<T>
-  export function useVModel<T>(props: any, key: string, emit: any, options: { passive?: boolean; deep?: boolean }): WritableComputedRef<T>
-}
